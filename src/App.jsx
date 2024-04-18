fcc5f6d9d822703f74e847412448189567490dc5
Anuj Nirmal <anujrajeshnirmal@gmail.com>
2024-04-18 20:16:10 +0530

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import authService from './appwrite/auth';
+import {login, logout} from "./store/authSlice";
 import './App.css'
 
 function App() {
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if(userData){
+          dispatch(login({userData}))
+        }else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false))
+  },[])
 
-  return (
-    <>
-     <h1>A blog with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className=''></div>
+  ) : null;
 }
 
 export default App
